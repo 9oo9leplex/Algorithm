b0170d5c3493b6b5ba7016590816a0497ef5ac2e
연창모 <ruderp@naver.com>
2023-01-04 18:50:48 +0900

    230104,Programmers_Level2,올바른괄호

---
+++ Algorithm/src/programmers/코딩테스트연습/level2/스택큐/올바른괄호.java
@@ -0,0 +1,21 @@
+package programmers.코딩테스트연습.level2.스택큐;
+
+public class 올바른괄호 {
+/*
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+
+        int ck = 0;
+        for(char c : s.toCharArray()){
+            if(c == '(') ck++;
+            else ck--;
+            
+            if(ck < 0 ) return false;
+        }
+        if(ck != 0) return false;
+        return true;
+    }
+}
+ */
+}
