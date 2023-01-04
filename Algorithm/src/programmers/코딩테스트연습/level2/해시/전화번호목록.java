8bdd383f96d0d8c3d6f7a20abfaa3db756740da2
연창모 <ruderp@naver.com>
2023-01-04 16:10:06 +0900

    230104,Programmers_Level2,전화번호목록

---
+++ Algorithm/src/programmers/코딩테스트연습/level2/해시/전화번호목록.java
@@ -0,0 +1,28 @@
+package programmers.코딩테스트연습.level2.해시;
+
+public class 전화번호목록 {
+/*
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        HashMap<String, Integer> hm = new HashMap<>();
+        char c;
+        int idx=0;
+        
+        for(String str : phone_book) hm.put(str,idx++);
+        
+
+        int len = phone_book.length;
+        for(int i=0;i<len;i++){
+            idx = phone_book[i].length();
+            for(int j=1;j<idx;j++){
+                // System.out.println(phone_book[i]);
+                if(hm.containsKey(phone_book[i].substring(0,j))) return false;
+            }
+        }
+        return true;
+    }
+}
+ */
+}
