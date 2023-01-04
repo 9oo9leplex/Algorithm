b08638b14f1dd6a538bc65455d044f75bdba4693
연창모 <ruderp@naver.com>
2023-01-04 19:27:16 +0900

    230104,Programmers_Level2,프린터

---
+++ Algorithm/src/programmers/코딩테스트연습/level2/스택큐/프린터.java
@@ -0,0 +1,32 @@
+package programmers.코딩테스트연습.level2.스택큐;
+
+public class 프린터 {
+/*
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        
+        int tmp = 0,target = 0, cnt = 0, len = priorities.length;
+        
+        for(int j=9;j>0;j--){
+            for(int i=0;i<len;i++){
+                if(priorities[(i+target)%len] == j){
+                    tmp = (i+target)%len;
+                    priorities[tmp] = 0;
+                    cnt++;
+                    if(tmp == location) return cnt;
+                    // System.out.println("배열: "+Arrays.toString(priorities));
+                    // System.out.println("인덱스: "+tmp);
+                    // System.out.println("카운트: "+cnt);
+                }
+            }
+            target = tmp;
+        }
+        
+        
+        int answer = 0;
+        return answer;
+    }
+}
+ */
+}
