85b5e483b743230aa39ab02fcb40784b3fb6a18d
연창모 <ruderp@naver.com>
2023-01-05 15:40:00 +0900

    230105,Programmers_Level1,K번째수

---
+++ Algorithm/src/programmers/코딩테스트연습/level1/정렬/K번째수.java
@@ -0,0 +1,29 @@
+package programmers.코딩테스트연습.level1.정렬;
+
+public class K번째수 {
+/*
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[commands.length];
+        int idx = 0;
+        
+        for(int[] command : commands){
+            command[0]--;
+            // command[1]--;
+            command[2]--;
+            
+            for(int i=command[0];i<command[1];i++) list.add(array[i]);
+            Collections.sort(list);
+            answer[idx++] = list.get(command[2]);
+            list.clear();
+        }
+        
+        
+        return answer;
+    }
+}
+ */
+}
