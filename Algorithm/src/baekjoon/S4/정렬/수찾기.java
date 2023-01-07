eaac4f2f1d99174af729d31de08bf2f0402248a2
연창모 <9oo9leplex@gmail.com>
2023-01-07 17:46:06 +0900

    feat,230107,baekjoonS4,정렬,수찾기

--- Algorithm/src/baekjoon/S4/정렬/수찾기.java
+++ Algorithm/src/baekjoon/S4/정렬/수찾기.java
@@ -1,4 +1,4 @@
-package baekjoon.G5.정렬;
+package baekjoon.S4.정렬;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException{
+public class 수찾기 {
+/*
+public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -34,29 +34,11 @@ public class Main {
 		
 		while(l <= r) {
 			mid = (l + r) / 2;
-//			System.out.println("[l,mid,r,val,cur]: ["+l+","+mid+","+r+","+arr[mid]+","+cur+"]");
 			if(arr[mid] < cur) l = mid + 1;
 			else if(arr[mid] > cur)	r = mid - 1;
 			else return 1;
 		}
 		return 0;
 	}
-	
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
