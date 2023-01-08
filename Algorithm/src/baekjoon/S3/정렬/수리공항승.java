0050478f86c7a8477b0eba7fce73246ace5109fb
연창모 <9oo9leplex@gmail.com>
2023-01-08 21:38:44 +0900

    feat,230108,baekjoonS3,정렬,수리공항승

--- Algorithm/src/baekjoon/S3/정렬/수리공항승.java
+++ Algorithm/src/baekjoon/S3/정렬/수리공항승.java
@@ -1,4 +1,4 @@
-package baekjoon.G5.정렬;
+package baekjoon.S3.정렬;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException{
+public class 수리공항승 {
+/*
+public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,22 +35,5 @@ public class Main {
 		}
 		System.out.println(answer);
 	}
-	
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
