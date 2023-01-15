0f6d6f56d7b034fbe69135b7f8de1ea7e56b35b2
연창모 <ruderp@naver.com>
2023-01-15 23:19:15 +0900

    feat,230114,baekjoonS3,탐욕,등수매기기2012

--- Algorithm/src/baekjoon/Main.java
+++ Algorithm/src/baekjoon/Main.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
@@ -10,35 +11,19 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int[] arr = new int[26];
-		for(char c : br.readLine().toCharArray()) arr[c - 'A']++;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
 		
-		int cnt = 0;
-		StringBuilder sb = new StringBuilder();
-		char mid = 'A';
-		int len = 0;
+		for(int i=0;i<n;i++) arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		int sum = 0;
 		
-		for(int i=0;i<26;i++) {
-			if(arr[i] % 2 == 1) {
-				mid = (char)(65 + i);
-				cnt++;
-			}
-			if(cnt >= 2) {
-				System.out.println("I'm Sorry Hansoo");
-				return;
-			}
-			len = sb.length();
-			for(int j=0;j<arr[i]/2;j++) {
-				sb.insert(len/2, (char)(65+i));
-				sb.insert(len/2+1,(char)(65+i));
-			}
+		for(int i=1;i<=n;i++) {
+			
 		}
 		
-		if(cnt == 1) {
-			len = sb.length();
-			sb.insert(len/2, mid);
-		}
-		System.out.println(sb);
+		System.out.println(sum);
+		
 	}
 	
 	
