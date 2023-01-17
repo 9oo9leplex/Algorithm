de7ac7f2dfa351be11b4de26bfd7c05653067d70
연창모 <ruderp@naver.com>
2023-01-17 16:57:05 +0900

    feat,230117,baekjoonS3,탐욕,최고의피자5545

--- Algorithm/src/baekjoon/S4/탐욕/최고의피자5545.java
+++ Algorithm/src/baekjoon/S4/탐욕/최고의피자5545.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.S4.탐욕;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class 최고의피자5545 {
+/*
+public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -36,27 +36,5 @@ public class Main {
 		}
 		System.out.println(answer);
 	}
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
