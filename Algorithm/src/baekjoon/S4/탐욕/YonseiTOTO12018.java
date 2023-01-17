aa9b5d109be5f9a3ce94fb954de7fc9055650709
연창모 <ruderp@naver.com>
2023-01-17 16:33:58 +0900

    feat,230117,baekjoonS3,YonseiTOTO12018

--- Algorithm/src/baekjoon/S4/탐욕/YonseiTOTO12018.java
+++ Algorithm/src/baekjoon/S4/탐욕/YonseiTOTO12018.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.S4.탐욕;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class YonseiTOTO12018 {
+/*
+public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -47,27 +47,5 @@ public class Main {
 		}
 		System.out.println(answer);
 	}
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
