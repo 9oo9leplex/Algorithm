bb6d2c320b5bdf33b63d268c8d1d152c4a58bbcc
연창모 <ruderp@naver.com>
2023-01-17 15:58:15 +0900

    feat,230117,baekjoonS3,탐욕,등수매기기2012

--- Algorithm/src/baekjoon/S4/탐욕/등수매기기2012.java
+++ Algorithm/src/baekjoon/S4/탐욕/등수매기기2012.java
@@ -1,13 +1,13 @@
-package baekjoon;
+package baekjoon.S4.탐욕;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class 등수매기기2012 {
+/*
+public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -23,27 +23,5 @@ public class Main {
 		}
 		System.out.println(sum);
 	}
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
