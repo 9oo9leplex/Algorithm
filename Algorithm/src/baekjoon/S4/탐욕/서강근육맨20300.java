019fe12611b0ef4325a82ce4d2deea4c092d5cc3
연창모 <ruderp@naver.com>
2023-01-17 17:19:40 +0900

    feat,230117,baekjoonS3,탐욕,서강근육맨20300

--- Algorithm/src/baekjoon/S4/탐욕/서강근육맨20300.java
+++ Algorithm/src/baekjoon/S4/탐욕/서강근육맨20300.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.S4.탐욕;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class 서강근육맨20300 {
+/*
+public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -33,27 +33,5 @@ public class Main {
 		}
 		System.out.println(answer);
 	}
+ */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
